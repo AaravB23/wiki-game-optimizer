5ab638310516e016c1a7662848484ef9b823f9df
AaravB23 <aaravbhatt76223@gmail.com>
2026-09-01 22:44:14 -0400

    added links & moved email to env

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import wikipediaapi as wikiapi
 import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+email = os.getenv("EMAIL")
 
 async def main():
     wiki = wikiapi.AsyncWikipedia(
-            user_agent='WikiRabbitHole (aaravbhatt76223@gmail.com)', language='en')
-    page_py = wiki.page('Python_(programming_language)')
+            user_agent="WikiRabbitHole (%s)" % email, language='en')
+    page_py = wiki.page('ASCC1')
     print("Page - Exists: %s" % await page_py.exists())
 
-    print(await page_py.text)
+    links = await page_py.links
+    for title in sorted(links.keys()):
+        print("%s: %s" % (title, links[title]))        
 
 asyncio.run(main())
